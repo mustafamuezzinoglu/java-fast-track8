ec0ebacb2573652bb8257866fb22f13cee18ee9b
Mustafa <mustafamuezzinoglu@gmail.com>
2024-07-25 00:52:43 +0200

    g

--- src/m13_exceptions/CheckedException.java
+++ src/m13_exceptions/CheckedException.java
@@ -1,4 +1,19 @@
 package m13_exceptions;
+/*
 
+ */
 public class CheckedException {
+    public static void main(String[] args) {
+        System.out.println("before thread.sleep()");
+
+        try {
+            Thread.sleep(2000);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }catch (Exception e) {
+            System.out.println("Caught an exception: " + e.getMessage());
+        }finally {
+            System.out.println("Finally clean up block");
+        }
+    }
 }
--- src/m13_exceptions/TryCatch.java
+++ src/m13_exceptions/TryCatch.java
@@ -1,2 +1,39 @@
-package m13_exceptions;public class TryCatch {
+package m13_exceptions;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        System.out.println("Execution started");
+
+        String word  = "java";
+        System.out.println(word.charAt(0));
+
+        try{
+            System.out.println(word.charAt(5));
+        }catch (StringIndexOutOfBoundsException e){
+//        }catch (RuntimeException e){
+            System.out.println("Exception caught and handled");
+        }
+
+        System.out.println("after word.charAT");
+
+        try {
+            System.out.println(10 / 0);
+        }catch (RuntimeException e){
+            System.out.println("Number 10 cannot / by zero");
+        }
+
+        System.out.println("After tray catch of / by zero");
+
+        try {
+        int[] nums = {3,6,1,5};
+        System.out.println(nums[2]);
+    }catch (Exception e) {
+            System.out.println("Exception caught with message = " +e.getMessage());
+        }finally {
+            System.out.println("Finally Block");
+        }
+
+    }
+
+
 }
